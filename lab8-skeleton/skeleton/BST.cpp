6e874fc3fcede83799878b235d7c3baa314f5340
kcmau <132557080+godsmartdog@users.noreply.github.com>
2025-11-18 13:50:50 +0000

    2

--- lab8-skeleton/skeleton/BST.cpp
+++ lab8-skeleton/skeleton/BST.cpp
@@ -19,19 +19,9 @@ int compare_by_length(const Song& one, const Song& two){
  * 0 if they are equivalent, or > 0 if the name of the first Song is lexicographically after the name of the second.
  */
 int compare_by_name(const Song& one, const Song& two){
-   int tmp = compare_by_length(one, two);
-   int sizel;
-   bool flag=false;
-    if(tmp==-1){
-        sizel=one.length;
-    }
-    else if(tmp==1) {
-        sizel=two.length;
-    }
-    else{
-        sizel =two.length;
-        flag=true;
-    }
+  
+   int sizel = min(one.name.length(), two.name.length());
+   
    
     for(int i=0; i < sizel;i++){
         if(one.name[i]<two.name.at(i)){
@@ -41,10 +31,15 @@ int compare_by_name(const Song& one, const Song& two){
             return 1;
         }
     }
-    if(flag){
+    if(one.name.length()<two.name.length()){
+        return -1;
+    }
+    else if(one.name.length()>two.name.length()){
+        return 1;
+    }
+    else{
         return 0;
     }
-    return (sizel==one.length?-1:1);
 }
 
 
@@ -53,4 +48,28 @@ int compare_by_name(const Song& one, const Song& two){
  * Returns an integer < 0 if the artist of the first Song is lexicographically before the name of the second,
  * 0 if they are equivalent, or > 0 if the artist of the first Song is lexicographically after the name of the second.
  */
-int compare_by_artist(const Song&, const Song&){}
+int compare_by_artist(const Song& one, const Song& two){
+    int sizel = min(one.artist.length(), two.artist.length());
+   
+   
+    for(int i=0; i < sizel;i++){
+        if(one.artist[i]<two.artist.at(i)){
+            return -1;
+        }
+        if(two.artist[i]<one.artist[i]){
+            return 1;
+        }
+    }
+    if(one.artist.length()<two.artist.length()){
+        return -1;
+    }
+    else if(one.artist.length()>two.artist.length()){
+        return 1;
+    }
+    else{
+        return 0;
+}}
+
+BST::BST(int(**compare_funcs)(const Song& ,const Song&),int value):root(nullptr), compare_funcs(compare_funcs),func_length(value){
+
+}
