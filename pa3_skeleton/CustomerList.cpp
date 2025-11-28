35c239170e0c73c2451adc5371cf4344da997d8b
kcmau <132557080+godsmartdog@users.noreply.github.com>
2025-11-28 12:39:14 +0000

    2.1

--- pa3_skeleton/CustomerList.cpp
+++ pa3_skeleton/CustomerList.cpp
@@ -1,1 +1,34 @@
 #include "CustomerList.h"
+
+CustomerList:: CustomerList(const CustomerList& other):head(nullptr),tail(nullptr),size(other.size){
+    CustomerNode *cur = other->head;
+    while(cur != nullptr){
+        add(other->data);
+        cur=cur->next;
+    }
+}
+void CustomerList::add(Customer* customer) {
+    CustomerNode* newNode = new CustomerNode(customer);
+    
+    if (head == nullptr) {
+      
+        head = newNode;
+        tail = newNode;
+    } else {
+      
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+    size++;
+}
+
+void CustomerList::add(const CustomerList* customers) {
+    if (customers == nullptr) return;
+    
+    CustomerNode* current = customers->head;
+    while (current != nullptr) {
+        add(current->data);  
+        current = current->next;
+    }
+}
