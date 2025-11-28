09080fbb2fa2428cfd67ef32a19c198549dccecf
kcmau <132557080+godsmartdog@users.noreply.github.com>
2025-11-28 09:48:58 +0000

    1.1

--- pa3_skeleton/Flight.cpp
+++ pa3_skeleton/Flight.cpp
@@ -1,1 +1,5 @@
 #include "Flight.h"
+Flight::Flight(const string& num, const string& dept, const string& dest):flightNumber(num), departure(dept), destination(dest), tickets(nullptr){
+   
+   cout<< toString()<<" has been created"<<endl;
+}
--- pa3_skeleton/Ticket.cpp
+++ pa3_skeleton/Ticket.cpp
@@ -8,3 +8,7 @@ string Ticket::toString() const {
   return info.flight->toString() + ", seat No.: " + to_string(info.seatNumber) + ", departure time: " 
     + stime + ", price: " + to_string(price);
 }
+
+Ticket:: Ticket(Flight* flight, int seatNum, const Datetime& time, int price):info(flight,seatNum,time),price(price){
+
+}
