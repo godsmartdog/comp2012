aaa4ef0dc7489012c4045792c097dcd02c4b79db
kcmau <132557080+godsmartdog@users.noreply.github.com>
2025-11-28 11:58:10 +0000

    1.6

--- pa3_skeleton/Transaction.cpp
+++ pa3_skeleton/Transaction.cpp
@@ -22,3 +22,16 @@ bool Transaction::operator== (const TransactionInfo& key) const {
     return info.ticket->getFlightInfo().departureTime == key.ticket->getFlightInfo().departureTime &&
            info.transID == key.transID;
 }
+
+int Transaction:: transNumber=0;
+ Transaction* Transaction:: makeTransaction(Customer* customer, Ticket* ticket, const Datetime& time){
+    if(customer==nullptr|| ticket == nullptr){
+        cout<< "Invalid transaction!"<<endl;
+        return nullptr;
+    }
+    Transaction *tmp= new Transaction(transNumber, ticket,time,customer->getDiscountedPercentage());
+    
+    cout<<"[Transaction "<<transNumber<<"]: "<< ticket->toString() <<" by "<< customer->getName()<<endl;
+    transNumber++;
+    return tmp;
+}
