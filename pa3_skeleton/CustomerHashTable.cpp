6e433765fefbe04fd0f578ad54e91365afdad9de
kcmau <132557080+godsmartdog@users.noreply.github.com>
2025-11-28 13:10:14 +0000

    whole2

--- pa3_skeleton/CustomerHashTable.cpp
+++ pa3_skeleton/CustomerHashTable.cpp
@@ -9,3 +9,43 @@ int CustomerHashTable:: hash(const string& name) const{
     sum= sum%size;
     return sum;
 }
+
+void CustomerHashTable:: add(Customer* customer){
+    if(customer==nullptr)
+    return;
+    int place = hash(customer->getName());
+    if(table[place].find(customer->getName())!=nullptr){
+        return;
+    }
+    table[place].add(customer);
+}
+
+Customer* CustomerHashTable:: get(const string& name) const{
+    int place = hash(name);
+    return table[place].find(name);
+}
+
+void CustomerHashTable:: remove(Customer* customer){
+    if(customer==nullptr)
+    return;
+    int place = hash(customer->getName());
+    if(table[place].find(customer->getName())==nullptr){
+        return;
+    }
+    table[place].remove(customer);
+}
+
+    // * Task 2.8: hash remove
+void CustomerHashTable:: clear(){
+    for (int i =0;i<size;i++){
+        table[i].clear();
+    }
+   
+}
+CustomerList* CustomerHashTable:: toList() const{
+    CustomerList *result= new CustomerList;
+    for (int i =0;i<size;i++){
+        result->add(table[i]);
+    }
+    
+}
